e81fb2b9ecf178569f25378f075ee27741f054dd
Pablo <pmartinez@quimerico.uy>
2022-03-16 12:08:18 -0300

    First commit

---
+++ src/components/pages/Home/index.tsx
@@ -0,0 +1,5 @@
+const IndexPage = () => {
+	return <h1 className="text-3xl font-bold underline">Start</h1>;
+};
+
+export default IndexPage;
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Head from "next/head";
+import Image from "next/image";
+import IndexPage from "../components/pages/Home";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+	return <IndexPage />;
+}
